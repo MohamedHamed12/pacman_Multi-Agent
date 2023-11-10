008454327cf99dd7b994dbc98d7d0c65c64b6c86
MohamedHamed12 <mh1779371@gmail.com>
2023-11-10 08:30:50 +0200

    reflex_agent

---
+++ multiagent/projectParams.py
@@ -0,0 +1,8 @@
+# projectParams.py
+# ----------------
+
+
+STUDENT_CODE_DEFAULT = 'multiAgents.py'
+PROJECT_TEST_CLASSES = 'multiagentTestClasses.py'
+PROJECT_NAME = 'Project 2: Multiagent search'
+BONUS_PIC = False
